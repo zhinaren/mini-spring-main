bdb356011091280c828ee23cd829bf75e5a62b70
wangc <3206922799@qq..com>
2026-08-02 11:03:54 +0800

    feat: mplement the initialization and destroy methods of Spring beans.

---
+++ src/main/java/org/springframework/beans/factory/config/SingletonBeanRegistry.java
@@ -0,0 +1,12 @@
+package org.springframework.beans.factory.config;
+
+public interface SingletonBeanRegistry {
+
+    Object getSingleton(String beanName);
+
+
+    /**
+     * 销毁单例对象
+     */
+    void destroySingletons();
+}
---
+++ src/main/java/org/springframework/context/ConfigurableApplicationContext.java
@@ -0,0 +1,18 @@
+package org.springframework.context;
+
+import org.springframework.beans.BeansException;
+
+public interface ConfigurableApplicationContext extends ApplicationContext {
+
+    /**
+     * 刷新容器
+     *
+     * @throws BeansException
+     */
+    void refresh() throws BeansException;
+
+    void registerShutdownHook();
+
+    void close();
+
+}
